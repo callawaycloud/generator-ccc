1a8fb2020e226d18dd6ed4d8f66f14e5960ffc31
Charlie Jonas <charlie@callawaycloudconsulting.com>
2019-12-23 14:41:36 -0700

    #12 merging package.xml

--- generators/app/index.ts
+++ generators/app/index.ts
@@ -1,10 +1,11 @@
 import Generator from "yeoman-generator";
 import yosay from "yosay";
 import path from "path";
-import {EOL} from 'os';
+import { EOL } from 'os';
 import merge from "deepmerge";
 import updateNotifier from 'update-notifier';
 import pkg from '../../package.json';
+import { parseStringPromise as parseXml } from 'xml2js';
 
 module.exports = class extends Generator {
   private props: {};
@@ -17,7 +18,7 @@ module.exports = class extends Generator {
     // Have Yeoman greet the user.
     this.log(yosay(`Callaway Cloud SFDX Project`));
 
-    const notifier = updateNotifier({pkg});
+    const notifier = updateNotifier({ pkg });
     notifier.notify();
 
     const prompts = [];
@@ -28,17 +29,32 @@ module.exports = class extends Generator {
     });
   }
 
-  public writing() {
+  public async writing() {
     this.fs.copy(
       this.templatePath(path.join(".", "static")),
       this.destinationPath("."),
       { globOptions: { dot: true } }
     );
 
+    await this.writeManifest();
     this.writeNpmPackage();
     this.writePrettier();
     this.writeVscodeSettings();
     this.writeGitIgnore();
+  }
+
+  private async writeManifest() {
+
+    const manifestPath = this.destinationPath(path.join("manifest","package.xml"));
+    const oldPkgManifest: any = this.fs.exists(manifestPath)
+      ? await readPackage(this.fs.read(manifestPath))
+      : null;
+
+    const defaultPath = this.templatePath(path.join(".", "dynamic", "package.xml"));
+    let defaultPkg = await readPackage(this.fs.read(defaultPath))
+
+    const newPkg = writePackage(merge(defaultPkg, oldPkgManifest));
+    this.fs.write(manifestPath, newPkg);
   }
 
   private writeNpmPackage() {
@@ -145,5 +161,55 @@ module.exports = class extends Generator {
     });
   }
 
-  end() {}
+  end() { }
 };
+
+
+// HELPER (move to new file)
+
+interface PackageXml {
+  version?: string;
+  namespace?: string;
+  types?: { [type: string]: string[] }
+}
+
+async function readPackage(xmlStr: string): Promise<PackageXml> {
+  let xml = await parseXml(xmlStr);
+  let version = xml?.Package?.version?.[0];
+  let namespace = xml?.Package?.$?.xmlns;
+  let types : { [type: string]: string[] };
+  if (xml.Package?.types) {
+    try{
+      types = xml.Package.types.reduce((res, t) => {
+        res[t.name[0]] = t.members;
+        return res;
+      }, {});
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  return {
+    version,
+    namespace,
+    types
+  };
+}
+
+function writePackage(pkg: PackageXml): string {
+  let types = '';
+  for(let key in pkg.types){
+    types += `  <types>\n`;
+    types += [...new Set<string>(pkg.types[key])].map( m => `    <members>${m}</members>`).join('\n') + '\n';
+    types += `    <name>${key}</name>\n`;
+    types += `  </types>\n`;
+  }
+
+
+  return (
+`<?xml version="1.0" encoding="UTF-8" ?>
+<Package xmlns="${pkg.namespace}">
+${types}  <version>${pkg.version}</version>
+</Package>
+`)
+}
