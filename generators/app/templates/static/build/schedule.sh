26458b155a0e6b6648e2eea3e564d159ac9afeb0
Dustin Kovac <fishygeek91@gmail.com>
2020-04-20 12:54:38 -0600

    - edited .schedule

--- generators/app/templates/static/build/schedule.sh
+++ generators/app/templates/static/build/schedule.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-
-log="$(git log --all --max-count=1 --format=%ct --grep='Auto-Pull of Production')"
+set -x;
+secondsSinceSync="$(git log --all --max-count=1 --format=%ct --grep='Auto-Pull of Production')"
 today=$(date +%s)
-numDays=$(($((today-log))/60/60/24))
+numDays=$(($((today-secondsSinceSync))/60/60/24))
 echo "The last sync was "$(git log --all --max-count=1 --format=%cr --grep='Auto-Pull of Production')
 
 if [ -z "$PRODUCTION_SYNC_INTERVAL" ]
 then
   PRODUCTION_SYNC_INTERVAL=3
 fi
-echo "The PRODUCTION_SYNC_INTERVAL is $PRODUCTION_SYNC_INTERVAL"
+echo "The PRODUCTION_SYNC_INTERVAL is $PRODUCTION_SYNC_INTERVAL days"
 if [ "$numDays" -lt "$PRODUCTION_SYNC_INTERVAL" ]
 then
-  echo "Sync is current. Exiting now."
+  echo "=== Sync is current. Exiting now. ==="
+  echo "*** Next Sync Schedule in $((PRODUCTION_SYNC_INTERVAL-numDays)) days."
 else
   echo "=== Sync about to begin... ==="
   ./setup.sh
@@ -20,3 +21,4 @@ else
   echo "=== Sync Complete ==="
   echo "*** Next Sync Schedule in $PRODUCTION_SYNC_INTERVAL days."
 fi
+set +x;
